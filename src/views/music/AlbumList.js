aa262976543c216ca77e0da7cc233f2b0136d04a
kimgeunha <antantsuper@gmail.com>
2024-04-27 08:54:39 +0900

    2024-04-27

--- src/views/music/AlbumList.js
+++ src/views/music/AlbumList.js
@@ -56,8 +56,14 @@ const AlbumList = () => {
 
   // 날짜가 선택될 때 호출될 콜백 함수
   const handleDateChange = date => {
+    debugger;
     //console.log(date);
     setSelectedDate(date);
+    if(!date){
+      return;
+    }
+
+    
     const localDate = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)); // 로컬 시간대로 조정
     const formattedDate = localDate.toISOString().slice(0, 10);
     
@@ -67,6 +73,11 @@ const AlbumList = () => {
   }
   const handleDateChange2 = date => {
     setSelectedDate2(date);
+    if(!date){
+      return;
+    }
+
+    
     const localDate = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)); // 로컬 시간대로 조정
     const formattedDate = localDate.toISOString().slice(0, 10);
     setAlbumSearch({ ...albumSearch, endReleaseDate: formattedDate })
