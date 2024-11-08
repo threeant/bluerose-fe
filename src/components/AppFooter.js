b39cd9c283db9dae5c44ca65705dcc8d46a0d375
kimgeunha <antantsuper@gmail.com>
2024-11-08 21:34:35 +0900

    v.0.1

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -8,7 +8,7 @@ const AppFooter = () => {
         {/* <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
           CoreUI
         </a> */}
-        <span className="ms-1">&copy; 2024. threeant all rights reserved.</span>
+        <span className="ms-1">&copy; 2024. threeant all rights reserved.  v.0.1</span>
       </div>
       <div className="ms-auto">
         {/* <span className="me-1">Powered by</span>
